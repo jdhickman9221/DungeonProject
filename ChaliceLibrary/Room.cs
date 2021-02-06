0d3ebc3ac0bcb363f696be43f9636279be48a81e
Jordan Hickman <jdhickman816@outlook.com>
2021-02-05 21:36:39 -0600

    first commit

---
+++ ChaliceLibrary/Room.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChaliceLibrary
+{
+    public class Room //TODO Create Room method
+    {
+        public static string GetRoom()
+        {
+            string[] rooms =
+            {
+               //TODO populate room lore
+
+            };
+            return rooms[new Random().Next(rooms.Length)];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+    }//endclass
+}//endnamespace
